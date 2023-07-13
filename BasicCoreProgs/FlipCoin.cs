e8e1384e8ffa064edbf680b17d9eb8c0dd00d5a4
S Soundarya <soundarya.s@tvsnext.com>
2023-07-13 22:00:19 +0530

    [Soundarya]Add: Added flip coin class.

---
+++ BasicCoreProgs/FlipCoin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjects
+{
+    public class Coin
+    {
+        Random random = new Random();
+        public void flip(int n)
+        {
+            int tails = 0, heads = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double coinflip = random.NextDouble();
+
+
+                if (coinflip < 0.5)
+                {
+                    tails++;
+                }
+                else
+                {
+                    heads++;
+                }
+
+            }
+
+            Console.WriteLine((heads * 100) / n);
+            Console.WriteLine((tails * 100) / n);
+
+        }
+    }
+
+}
--- BasicCoreProgs/Program.cs
+++ BasicCoreProgs/Program.cs
@@ -7,6 +7,18 @@ namespace MiniProjects
         public static void Main(string[] args)
         {
             Console.WriteLine("Choose one:\n1.Coin\n2.Leap year\n3.Power of 2\n4.Harmonic number\n5.Factors\n6.Quotient&remainder\n7.Swap\n8.Even or odd\n9.Vowel or Consonant\n10.Largest among three.");
-        }
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Coin coin = new Coin();
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter how many times to flip the coin:");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    coin.flip(n);
+                    break;
+            }
+            }
     }
 }
