1934320af895274c84d6e2642fbe46014097f5b9
S Soundarya <soundarya.s@tvsnext.com>
2023-07-13 21:56:13 +0530

    [Soundarya]: first commit.

---
+++ BasicCoreProgs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniProjects
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Choose one:\n1.Coin\n2.Leap year\n3.Power of 2\n4.Harmonic number\n5.Factors\n6.Quotient&remainder\n7.Swap\n8.Even or odd\n9.Vowel or Consonant\n10.Largest among three.");
+        }
+    }
+}
